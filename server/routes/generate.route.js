b1aed056a29d955d0622d99e8c2c11ad84a235a1
soumaychoudhary <wultimate646@gmail.com>
2026-08-12 19:23:39 +0530

    Fix Bugs

--- server/routes/generate.route.js
+++ server/routes/generate.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js";
 import { generateNotes } from "../controllers/generate..controller.js";
+import { getMyNotes, getSingleNotes } from "../controllers/notes.controller.js"
  
 
 const notesRouter = express.Router();
